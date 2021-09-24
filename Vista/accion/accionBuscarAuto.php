70e8f7a33915f7ea1cdfd9342ca26bf89fa10336
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-09-24 09:47:34 -0300

    implementada la salida de datos del cliente al buscar auto por patente o dni

--- Vista/accion/accionBuscarAuto.php
+++ Vista/accion/accionBuscarAuto.php
@@ -35,18 +35,47 @@ echo '<h1>' . $encabezado . '</h1>';
 echo '<p>' . $enunciado . '</p>';
 
 if (count($autoBuscado) > 0) {
+    if (isset($datos['patente'])) {
+        echo '<div class="row">
+        <div class="col border border-secondary text-light bg-dark"><strong>DNI</strong></div>
+        <div class="col border border-secondary text-light bg-dark"><strong>NOMBRE Y APELLIDO</strong></div>
+        <div class="col border border-secondary text-light bg-dark"><strong>FECHA NACIMIENTO</strong></div>
+        <div class="col border border-secondary text-light bg-dark"><strong>TELEFONO</strong></div>
+        <div class="col border border-secondary text-light bg-dark"><strong>DOMICILIO</strong></div>
+        </div>';
+    
+        echo '<div class="row">
+        <div class="col border border-secondary">' . $autoBuscado[0]->getObjDuenio()->getNroDni() . '</div>
+        <div class="col border border-secondary">' . $autoBuscado[0]->getObjDuenio()->getNombre() . ' ' . $autoBuscado[0]->getObjDuenio()->getApellido() . '</div>
+        <div class="col border border-secondary">' . $autoBuscado[0]->getObjDuenio()->getFechaNac() . '</div>
+        <div class="col border border-secondary">' . $autoBuscado[0]->getObjDuenio()->getTelefono() . '</div>
+        <div class="col border border-secondary">' . $autoBuscado[0]->getObjDuenio()->getDomicilio() . '</div>
+        </div>';
+    }
+
+    if (isset($datos['dniDuenio'])) {
+        echo '<div class="row">
+        <div class="col border border-secondary text-light bg-dark"><strong>NOMBRE Y APELLIDO</strong></div>
+        </div>';
+    
+        echo '<div class="row">
+        <div class="col border border-secondary">' . $autoBuscado[0]->getObjDuenio()->getNombre() . ' ' . $autoBuscado[0]->getObjDuenio()->getApellido() . '</div>
+        </div>';
+    }
+    
+
+    echo '<br><br>';
+
     echo '<div class="row">
 <div class="col border border-secondary bg-info"><strong>PATENTE</strong></div>
 <div class="col border border-secondary bg-info"><strong>MARCA</strong></div>
 <div class="col border border-secondary bg-info"><strong>MODELO</strong></div>
-<div class="col border border-secondary bg-info"><strong>DUEÑO</strong></div>
 </div>';
     foreach ($autoBuscado as $objAuto) {
         echo '<div class="row">
         <div class="col border border-secondary">' . $objAuto->getPatente() . '</div>
         <div class="col border border-secondary">' . $objAuto->getMarca() . '</div>
         <div class="col border border-secondary">' . $objAuto->getModelo() . '</div>
-        <div class="col border border-secondary">' . $objAuto->getObjDuenio()->getNombre() . ' ' . $objAuto->getObjDuenio()->getApellido() . '</div>
         </div>';
     }
 
